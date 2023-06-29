084c7cec3af97ccf37dcad34a05df0847488fd00
Shubham Baliram Songire <sbs050501@gmail.com>
2023-06-29 16:46:40 +0530

    Update Demo.py

--- Vehicle Classification and counting/Demo.py
+++ Vehicle Classification and counting/Demo.py
@@ -1,19 +1,12 @@
-
-
-
 from detection_helpers import *
 from tracking_helpers import *
 from  bridge_wrapper import *
 from PIL import Image
 
-
-
 detector = Detector(classes = None) 
 detector.load_model(r"last.pt")
 
-
 # # RUN
 tracker = YOLOv7_DeepSORT(reID_model_path="./deep_sort/model_weights/mars-small128.pb", detector=detector)
-
 
 tracker.track_video(r"11.mp4", output=r"video2.mp4",show_live = True, skip_frames = 0, count_objects = True, verbose=1)
